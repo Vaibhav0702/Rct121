09849d794754f26e46ef0e0920f50c5cd37fe590
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-05-18 12:29:40 +0530

    google timer

---
+++ google_timer/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import {Timer} from "./conponents/Timer"
+
+
+function App() {
+  return (
+    <div className="App">
+       
+      
+      <Timer/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ google_timer/src/conponents/Timer.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+
+import { Showtimer } from "./Showtimer"
+
+import { Controlbuttons } from "./Controlbuttons"
+
+export const Timer = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const [isPaused, setIsPaused] = useState(true);
+
+    const [time, setTime] = useState(0);
+    
+
+    useEffect(() => {
+
+        let interval = null;
+
+        if (isActive && isPaused === false) {
+
+            interval = setInterval(() => {
+
+                setTime((time) => time + 10);
+
+            }, 10);
+
+        } else {
+
+            clearInterval(interval);
+
+        }
+        return () => {
+
+            clearInterval(interval);
+
+        };
+    }, [isActive, isPaused]);
+
+
+    const handleStart = () => {
+        setIsActive(true);
+        setIsPaused(false);
+    };
+
+    const handlePauseResume = () => {
+        setIsPaused(!isPaused);
+    };
+
+    const handleReset = () => {
+        setIsActive(false);
+        setTime(0);
+    };
+
+    return (
+        <div>
+            <h1 style={{ color: "tomato", fontSize: "40px", fontStyle: "italic", }}>Google Timer</h1>
+
+            <Showtimer time={time} />
+
+            <Controlbuttons
+                active={isActive}
+                isPaused={isPaused}
+                handleStart={handleStart}
+                handlePauseResume={handlePauseResume}
+                handleReset={handleReset}
+            />
+
+        </div>
+    )
+}
