a4363b9cf236d400a459fa16f5953648d42ce927
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-05-22 21:36:01 +0530

    Tic-Tac-Toe

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { TicTacToe } from './Component/Tic-Tac-Toe';
+
+function App() {
+  return (
+    <div className="App">
+          
+          <TicTacToe/>
+
+    </div>
+  );
+}
+
+export default App;
